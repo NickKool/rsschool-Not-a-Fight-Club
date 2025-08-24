7463714b62b5d08a884e87663e0a3b6eabac580a
Nikolay <nickcooluxui@gmail.com>
2025-08-24 23:51:28 +0300

    feat:add window for choose avatar

--- fightclub/basic.js
+++ fightclub/basic.js
@@ -10,3 +10,30 @@
   if (username) {
     document.getElementById("character").textContent = `${username}`;
   } 
+
+
+const modal = document.getElementById("avatarModal");
+const playerAvatar = document.getElementById("playerAvatar");
+const span = modal.querySelector(".close");
+const avatars = document.querySelectorAll(".avatar");
+
+playerAvatar.onclick = () => {
+  modal.style.display = "flex";
+}
+
+span.onclick = () => {
+  modal.style.display = "none";
+}
+
+avatars.forEach(img => {
+  img.onclick = () => {
+    playerAvatar.src = img.src;              
+    localStorage.setItem("avatar", img.src); 
+    modal.style.display = "none";            
+  }
+});
+
+window.onload = () => {
+  const savedAvatar = localStorage.getItem("avatar");
+  if (savedAvatar) playerAvatar.src = savedAvatar;
+}
