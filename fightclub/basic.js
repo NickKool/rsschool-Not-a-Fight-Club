a9f04e025a2f62f9964cd5c7ed810be01f99b84a
Nikolay <nickcooluxui@gmail.com>
2025-08-28 00:10:50 +0300

    fix:delete new function on tha index page

--- fightclub/basic.js
+++ fightclub/basic.js
@@ -10,9 +10,6 @@
   if (username) {
     document.getElementById("character").textContent = `${username}`;
   } 
-  if (window.location.pathname.includes("index.html") && username) {
-  window.location.href = "index1.html";
-  }
 
 const modal = document.getElementById("avatarModal");
 const playerAvatar = document.getElementById("playerAvatar");
